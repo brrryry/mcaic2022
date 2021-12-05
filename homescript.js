ba3fe4ca1043ae26694933427764a759718b1aea
YouLikeCats <77174200+YouLikeCats@users.noreply.github.com>
2021-12-05 17:34:56 -0500

    Official Domain

--- homescript.js
+++ homescript.js
@@ -5,22 +5,35 @@ $(function(){  // $(document).ready shorthand
 
 $(document).ready(function() {
 
-    $('.fadein').each(function(i) {
-      var object_threshold = ($(this).position().top + $(this).position().top + $(this).outerHeight()) / 2;
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
+  $('.fadein').each(function(i) {
+    var object_threshold = ($(this).position().top + $(this).position().top + $(this).outerHeight()) / 2;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-      /* If the object is completely visible in the window, fade it it */
-      if( bottom_of_window > object_threshold){
+    /* If the object is completely visible in the window, fade it it */
+    if( bottom_of_window > object_threshold){
 
-          $(this).animate({'opacity':'1'}, 1500);
-          //$(this).animate(, 1500);
+        $(this).animate({'opacity':'1'}, 1500);
+        //$(this).animate(, 1500);
 
-      }
-    });
+    }
+  });
 
   /* Every time the window is scrolled ... */
   $(window).scroll( function(){
 
+
+      $('.fadein').each(function(i) {
+        var object_threshold = ($(this).position().top + $(this).position().top + $(this).outerHeight()) / 2;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > object_threshold){
+
+            $(this).animate({'opacity':'1'}, 1500);
+            //$(this).animate(, 1500);
+
+        }
+      });
       /* Check the location of each desired element */
       $('.hideme').each(function(i){
 
